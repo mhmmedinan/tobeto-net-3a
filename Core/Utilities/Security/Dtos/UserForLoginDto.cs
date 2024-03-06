7a5003d12cd4aa9e56267e473e7d54d92f594ee9
Muhammed İnan <minan.65@hotmail.com>
2024-03-06 16:42:19 +0300

    06.03.2024 Commit 2

---
+++ Core/Utilities/Security/Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Utilities.Security.Entities;
+
+namespace Core.Utilities.Security.Dtos;
+
+public class UserForLoginDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
---
+++ Core/Utilities/Security/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Core.Utilities.Security.Entities;
+
+public class User:BaseEntity<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public byte[] PasswordHash {  get; set; }
+    public byte[] PasswordSalt { get; set; }
+
+}
